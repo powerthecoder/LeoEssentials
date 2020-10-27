eb5b527a728629a65277b31ce4ca26dea7cb614f
Leo <leo@powerthecoder.xyz>
2020-10-27 00:07:06 -0600

    Update Main.java

--- src/xyz/powerthecoder/Essentials/Main.java
+++ src/xyz/powerthecoder/Essentials/Main.java
@@ -1,10 +1,14 @@
 package xyz.powerthecoder.Essentials;
 
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Main extends JavaPlugin implements Listener {
 	
@@ -19,6 +23,7 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
 		// about
 		if (label.equalsIgnoreCase("about")) {
 			if(!(sender instanceof Player)) {
@@ -34,6 +39,69 @@ public class Main extends JavaPlugin implements Listener {
 			player.sendMessage("GitHub: github.com/powerthecoder/");
 			player.sendMessage("  Website: powerthecoder.xyz/");
 			player.sendMessage("--------------------------------");
+		}
+		
+		
+		// GameMode
+		if (label.equalsIgnoreCase("gm")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("You are console. Your already in God Mode!");
+			}
+			if (args[0].equalsIgnoreCase("0")) {
+				Player player = (Player) sender;
+				GameMode mode = player.getGameMode();
+				player.setGameMode(GameMode.SURVIVAL);
+				player.sendMessage("You are now in Survival");
+			}
+			if (args[0].equalsIgnoreCase("1")) {
+				Player player = (Player) sender;
+				GameMode mode = player.getGameMode();
+				player.setGameMode(GameMode.CREATIVE);
+				player.sendMessage("You are now in Creative");
+			}
+			
+		}
+		if (label.equalsIgnoreCase("gmc")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("You are console. Your already in God Mode!");
+			}
+			Player player = (Player) sender;
+			GameMode mode = player.getGameMode();
+			player.setGameMode(GameMode.CREATIVE);
+			player.sendMessage("You are now in Creative");
+		}
+		if (label.equalsIgnoreCase("gms")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("You are console. Your already in God Mode!");
+			}
+			Player player = (Player) sender;
+			GameMode mode = player.getGameMode();
+			player.setGameMode(GameMode.SURVIVAL);
+			player.sendMessage("You are now in Survival");
+		}
+		
+		
+		// Speed Effect
+		if (label.equalsIgnoreCase("speed")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("Your Console... Go away");
+			}
+			Player player = (Player) sender;
+			String arg_1 = args[0];
+			int arg1 = Integer.parseInt(arg_1);
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999, arg1));
+            player.sendMessage("You are now fast :)");
+		}
+		
+		//BroadCast
+		if (label.equalsIgnoreCase("bc")) {
+			if(!(sender instanceof Player)) {
+				sender.sendMessage("Sending Broadcast...");
+				Bukkit.getServer().broadcastMessage(args[0]);
+			}
+			Player player = (Player) sender;
+			player.sendMessage("Sending Broadcast...");
+			Bukkit.getServer().broadcastMessage(args[0]);
 		}
 		
 		
